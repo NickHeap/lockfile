e83c723aca17705fc67ac635da383bc9afeddb29
Nick Heap <nick.heap@oneadvanced.com>
2018-02-15 12:45:01 +0000

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+
+namespace LockFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: Lockfile {filename}.");
+                return;
+            }
+
+            string filename = args[0];
+
+            Console.WriteLine(String.Format("Locking file: {0}...",filename));
+
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("!!! File not found !!!");
+                return;
+            }
+            
+            FileStream lockfile = File.Open(filename,FileMode.Open,FileAccess.Read,FileShare.None);
+            
+            Console.WriteLine("File locked. Press Return to unlock.");
+            
+            Console.ReadLine();
+            
+            Console.WriteLine("Closing file...");
+            lockfile.Close();
+            Console.WriteLine("File unlocked.");
+
+        }
+    }
+}
